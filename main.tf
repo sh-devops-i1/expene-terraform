de3558ea5db42fd31f8d304d301708e53d738ac6
shujath-git900 <shujath.devops@gmail.com>
2026-01-29 16:45:03 +0530

    creating backend and mysql

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ module "frontend" {
   ssh_user = var.ssh_user
   ssh_password = var.ssh_password
 }
+module "backend" {
+  source = "./modules/apps"
+  env    = var.env
+  instance_type = var.instance_type
+  component = "backend"
+  zone_id = var.zone_id
+  ssh_user = var.ssh_user
+  ssh_password = var.ssh_password
+}
+module "mysql" {
+  source = "./modules/apps"
+  env    = var.env
+  instance_type = var.instance_type
+  component = "mysql"
+  zone_id = var.zone_id
+  ssh_user = var.ssh_user
+  ssh_password = var.ssh_password
+}
