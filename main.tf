7305163e1a114d6b135cf8cef97c07e23ed39abc
shujath-git900 <shujath.devops@gmail.com>
2026-03-26 14:50:03 +0530

    commented to test

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-module "frontend" {
-  source = "./modules/apps"
-  env    = var.env
-  instance_type = var.instance_type
-  component = "frontend"
-  zone_id = var.zone_id
-  ssh_user = var.ssh_user
-  ssh_password = var.ssh_password
-}
-module "backend" {
-  source = "./modules/apps"
-  env    = var.env
-  instance_type = var.instance_type
-  component = "backend"
-  zone_id = var.zone_id
-  ssh_user = var.ssh_user
-  ssh_password = var.ssh_password
-}
+# module "frontend" {
+#   source = "./modules/apps"
+#   env    = var.env
+#   instance_type = var.instance_type
+#   component = "frontend"
+#   zone_id = var.zone_id
+#   ssh_user = var.ssh_user
+#   ssh_password = var.ssh_password
+# }
+# module "backend" {
+#   source = "./modules/apps"
+#   env    = var.env
+#   instance_type = var.instance_type
+#   component = "backend"
+#   zone_id = var.zone_id
+#   ssh_user = var.ssh_user
+#   ssh_password = var.ssh_password
+# }
 module "mysql" {
   source = "./modules/apps"
   env    = var.env
