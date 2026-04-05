56da46326ca20fc7adacd43adc55daa979c8194a
shujath-git900 <shujath.devops@gmail.com>
2026-04-05 17:35:28 +0530

    order adjusted

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,16 +5,6 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-vpc"
   }
 }
-resource "aws_subnet" "frontend_main" {
-  count             = length(var.frontend_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.frontend_subnets[count.index]
-  availability_zone = var.Availability_zones[count.index]
-
-  tags = {
-    Name = "${var.env}-frontend-subnets-${count.index}"
-  }
-}
 resource "aws_vpc_peering_connection" "peer" {
 
   peer_vpc_id   = var.default_vpc_id
@@ -36,5 +26,16 @@ resource "aws_route" "default" {
   route_table_id            = var.default_rt_ID
   destination_cidr_block    = var.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_subnet" "frontend_main" {
+  count             = length(var.frontend_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.frontend_subnets[count.index]
+  availability_zone = var.Availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env}-frontend-subnets-${count.index}"
+  }
 }
 
