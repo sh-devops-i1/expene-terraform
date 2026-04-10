179f8a1b4d4653e2851abbbb428413f1fcba0f9c
shujath-git900 <shujath.devops@gmail.com>
2026-04-10 21:51:52 +0530

    adding igw to the public subnet route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -126,10 +126,17 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_route_table" "public-rt" {
   count             = length(var.public_subnet)
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = var.default_vpc_cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   tags = {
     Name = "${var.env}-public-rt-${count.index + 1}"
   }
