df0aab50d1c84d7b8af825bc4cbe33bf49f086a2
shujath-git900 <shujath.devops@gmail.com>
2026-04-10 22:09:05 +0530

    count.index fix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "frontend-rt" {
   }
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw.id
+    gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 
 
